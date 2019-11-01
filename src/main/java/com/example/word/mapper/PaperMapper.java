2d5d43d350c3bc773609225c2f229ff4f8b93fc3
user.name <13028891745@163.com>
2019-11-01 10:46:34 +0800

    第一次修改

--- src/main/java/com/example/word/mapper/PaperMapper.java
+++ src/main/java/com/example/word/mapper/PaperMapper.java
@@ -2,8 +2,22 @@ package com.example.word.mapper;
 
 import com.example.word.pojo.Students;
 import org.apache.ibatis.annotations.Param;
+import java.util.List;
+import com.example.word.pojo.Paper;
+import com.example.word.pojo.Writea;
+import com.example.word.pojo.Mark;
 
 public interface PaperMapper {
     //学生登录
     public Students findStudents(@Param("semail")String semail, @Param("sname")String sname,@Param("spass")String spass);
+    //根据试卷号查询单词
+    public List<Paper> findPaperList(@Param("pnum")String pnum);
+    //根据试卷号查询除单词序号以外的其他字段
+    public Paper findPapername(@Param("pnum")String pnum);
+    //试卷号查询
+    public List<Paper> findPnum(@Param("stuid")int stuid);
+    //添加默写信息
+    public int addWritea(Writea wri);
+    //添加学生分数
+    public int addMark(Mark mark);
 }
