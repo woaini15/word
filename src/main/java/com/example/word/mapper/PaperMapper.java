184d2f63b6811d84ea5d1ee5996d168b43a8a606
user.name <13028891745@163.com>
2019-11-05 11:50:24 +0800

    第四次修改

--- src/main/java/com/example/word/mapper/PaperMapper.java
+++ src/main/java/com/example/word/mapper/PaperMapper.java
@@ -1,12 +1,13 @@
 package com.example.word.mapper;
 
 import com.example.word.pojo.Students;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import java.util.List;
 import com.example.word.pojo.Paper;
 import com.example.word.pojo.Writea;
 import com.example.word.pojo.Mark;
-
+@Mapper
 public interface PaperMapper {
     //学生登录
     public Students findStudents(@Param("semail")String semail, @Param("sname")String sname,@Param("spass")String spass);
